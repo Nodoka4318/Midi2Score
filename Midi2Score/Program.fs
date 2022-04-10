9132c2d39f48dd29dece49d0a9339f3e4b9dac62
Nodoka4318 <78198198+Nodoka4318@users.noreply.github.com>
2022-04-10 12:11:34 +0900

    プロジェクト ファイルを追加します。

---
+++ Midi2Score/Program.fs
@@ -0,0 +1,27 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+open Commons.Music.Midi
+
+let midi_reader path =
+    let access = MidiAccessManager.Default
+    let midi = MidiMusic.Read(System.IO.File.OpenRead(path))
+    let tracks = midi.Tracks
+    
+    printf "トラックを選択してください。"
+    for i in 0 .. tracks.Count do
+        printf $"[{i}]"
+
+[<EntryPoint>]
+let main argv =
+    printf @"
+┏━┓┏━┳━━┳━━━┳━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┓
+┃┃┗┛┃┣┫┣┻┓┏┓┣┫┣┫┏━┓┃┏━┓┃┏━┓┃┏━┓┃┏━┓┃┏━━┛
+┃┏┓┏┓┃┃┃╋┃┃┃┃┃┃┗┛┏┛┃┗━━┫┃╋┗┫┃╋┃┃┗━┛┃┗━━┓
+┃┃┃┃┃┃┃┃╋┃┃┃┃┃┃┏━┛┏┻━━┓┃┃╋┏┫┃╋┃┃┏┓┏┫┏━━┛
+┃┃┃┃┃┣┫┣┳┛┗┛┣┫┣┫┃┗━┫┗━┛┃┗━┛┃┗━┛┃┃┃┗┫┗━━┓
+┗┛┗┛┗┻━━┻━━━┻━━┻━━━┻━━━┻━━━┻━━━┻┛┗━┻━━━┛  by Nodoka
+        "
+    printf "\n>> midiファイルをここに落とすか、midiファイルへのフルパスを入力してください。"
+    0 // return an integer exit code
